2bb7fe647c168f88b607dd5d9ddffa9e8e96768a
김태훈 <gaetaeng@git.duzon.com>
2022-11-01 17:28:16 +0900

    footer css 추가

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,5 @@
 import React from "react"; 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./Header/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom"; 
 import MainContainer from "./MainContainer";
 
 function App() {
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,9 @@
 import React from "react"; 
+import "./Footer.css"
 
 export default function Footer({ children }) { 
     return (
-        <div style={{position:"absolute", bottom : "0px", height:"120px", background:"##8d8d8d", width :"100%", borderTop:"1px solid gray"}}>  
+        <div className="footer">  
             {children} 
         </div>
     )
