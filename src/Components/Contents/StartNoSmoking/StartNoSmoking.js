337b7a868638ccd32bdd31c6d8403aedc77f33e1
김태훈 <gaetaeng@git.duzon.com>
2022-11-02 13:43:14 +0900

    test 데이터 삽입

--- src/Components/Contents/StartNoSmoking/StartNoSmoking.js
+++ src/Components/Contents/StartNoSmoking/StartNoSmoking.js
@@ -1,16 +1,26 @@
+import dayjs from "dayjs";
 import React from "react";  
 import "./StartNoSmoking.css"
+
+
+
+const DATA_LIST = [
+    {title : "금연 시작 일", data : dayjs().format("YYYY년 MM월 DD일")},
+    {title : "금연 기간", data : "0일"},
+    {title : "피우지 않은 담배", data : "10,000 개비"},
+]
 
 export default function StartNoSmoking({ children }) { 
     return (
         <div class="container text-center">
             <div class="row row-cols-2">
-                <div class="col col-md-4 title">Column</div>
-                <div class="col col-md-8 content">Column</div>
-                <hr/><hr/>
-                <div class="col col-md-4 title">Column</div>
-                <div class="col col-md-8 content">Column</div>
-                <hr/><hr/>
+                {DATA_LIST.map(data => {
+                    return <div>
+                            <div class="col col-md-4 title">{data.title}</div>
+                                <div class="col col-md-8 content">{data.data}</div>
+                                <hr/>
+                            </div>
+                })} 
             </div>
             </div>
     )
