f5461b1170c80a03bc829e1dd6e015eebc046bb3
GaeTaeng <nuckly60@gmail.com>
2022-11-04 13:04:20 +0900

    playground 추가

---
+++ src/Components/Contents/PlayGround/PlayGround.js
@@ -0,0 +1,10 @@
+import React from "react"; 
+ 
+
+export default function PlayGround({ children }) { 
+    return (
+        <div>
+            마음대로 만들어두면 나중에 구조바꾸고 이런거 가르쳐줄게용
+        </div>
+    )
+}
