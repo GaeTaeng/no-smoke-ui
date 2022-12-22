347db595fb390ab9997e84a5aabe787d026a5e8e
GaeTaeng <nuckly60@gmail.com>
2022-12-22 11:36:57 +0900

    카카오 로그인 구현

--- src/Components/Header/Login/Login.js
+++ src/Components/Header/Login/Login.js
@@ -21,6 +21,9 @@ export default function Login({setUser}) {
 
       }
       useEffect(()=> {
+
+      // Kakao.init('744e06b2794760aa9989c6828987a345'); //발급받은 키 중 javascript키를 사용해준다.
+      // console.log(Kakao.isInitialized()); // sdk초기화여부판단
         const selectBoxElements = document.querySelectorAll(".select");
 
         selectBoxElements.forEach(selectBoxElement => {
@@ -36,20 +39,53 @@ export default function Login({setUser}) {
             });
           });
       }, [])
+
+
+
+      
+    // <!-- 카카오 스크립트 --> 
+    //카카오로그인
+    const KakaoLogin = () =>  {
+      const url = 'https://kauth.kakao.com/oauth/authorize?client_id=744e06b2794760aa9989c6828987a345' +
+          // process.env.VUE_APP_KAKAO_JS_KEY +
+          '&redirect_uri=https://gaetaeng.xyz/playground' +
+          // process.env.VUE_APP_KAKAO_REDIRECT_URL +
+          '&response_type=code&' 
+          window.location.href = url;
+	  
+    }
+    //카카오로그아웃  
+    // function kakaoLogout() {
+    //     if (Kakao.Auth.getAccessToken()) {
+    //       Kakao.API.request({
+    //         url: '/v1/user/unlink',
+    //         success: function (response) {
+    //           console.log(response)
+    //         },
+    //         fail: function (error) {
+    //           console.log(error)
+    //         },
+    //       })
+    //       Kakao.Auth.setAccessToken(undefined)
+    //     }
+    //   }   
+
+    
     return (
         <div className="login"> 
-            <div className="select">
-                <div className="selected ">
-                    <div className="selected-value">사용자를 선택하여주세요. </div>
-                    <div className="arrow"></div>
-                </div>
-                <ul>
-                    {testData.map((user, idx) => { 
-                        return <li key={idx} className="option" onClick={handleClickLogin} value={idx}>{user.name}</li>
 
-                    })}   
+<ul>
+                  <li onClick={() => KakaoLogin()}>
+                      <a href="javascript:void(0)">
+                          <span>카카오 로그인</span>
+                      </a>
+                  </li>
+                  {/* <li onclick={() => kakaoLogout()}>
+                      <a href="javascript:void(0)">
+                          <span>카카오 로그아웃</span>
+                      </a>
+                  </li> */}
                 </ul>
-            </div>  
         </div>
     )
 }
