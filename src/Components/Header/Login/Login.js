b5edac84224db7005039d71417d9be24e1215931
GaeTaeng <nuckly60@gmail.com>
2022-12-23 11:39:35 +0900

    로그인데이터 추가

--- src/Components/Header/Login/Login.js
+++ src/Components/Header/Login/Login.js
@@ -4,41 +4,41 @@ import { testData } from "../../TestData";
 import "./Login.css"
 export default function Login({setUser}) { 
 
-    const handleClickLogin = (e) => { 
-        setUser(testData[e.target.value])
-    }
-    function toggleSelectBox(selectBox) {
-        selectBox.classList.toggle("active");
-      }
+    // const handleClickLogin = (e) => { 
+    //     setUser(testData[e.target.value])
+    // }
+    // function toggleSelectBox(selectBox) {
+    //     selectBox.classList.toggle("active");
+    //   }
       
-      function selectOption(optionElement) {
-        const selectBox = optionElement.closest(".select");
-        const selectedElement = selectBox.querySelector(".selected-value");
-        selectedElement.textContent = optionElement.textContent;
-      }
+    //   function selectOption(optionElement) {
+    //     const selectBox = optionElement.closest(".select");
+    //     const selectedElement = selectBox.querySelector(".selected-value");
+    //     selectedElement.textContent = optionElement.textContent;
+    //   }
 
-      function handleClickSelectBox(e) {
+    //   function handleClickSelectBox(e) {
 
-      }
-      useEffect(()=> {
+    //   }
+    //   useEffect(()=> {
 
-      // Kakao.init('744e06b2794760aa9989c6828987a345'); //발급받은 키 중 javascript키를 사용해준다.
-      // console.log(Kakao.isInitialized()); // sdk초기화여부판단
-        const selectBoxElements = document.querySelectorAll(".select");
+    //   // Kakao.init('744e06b2794760aa9989c6828987a345'); //발급받은 키 중 javascript키를 사용해준다.
+    //   // console.log(Kakao.isInitialized()); // sdk초기화여부판단
+    //     const selectBoxElements = document.querySelectorAll(".select");
 
-        selectBoxElements.forEach(selectBoxElement => {
-            selectBoxElement.addEventListener("click", function (e) {
-              const targetElement = e.target;
-              const isOptionElement = targetElement.classList.contains("option");
+    //     selectBoxElements.forEach(selectBoxElement => {
+    //         selectBoxElement.addEventListener("click", function (e) {
+    //           const targetElement = e.target;
+    //           const isOptionElement = targetElement.classList.contains("option");
           
-              if (isOptionElement) {
-                selectOption(targetElement);
-              }
+    //           if (isOptionElement) {
+    //             selectOption(targetElement);
+    //           }
           
-              toggleSelectBox(selectBoxElement);
-            });
-          });
-      }, [])
+    //           toggleSelectBox(selectBoxElement);
+    //         });
+    //       });
+    //   }, [])
 
 
 
@@ -50,7 +50,8 @@ export default function Login({setUser}) {
           // process.env.VUE_APP_KAKAO_JS_KEY +
           '&redirect_uri=https://gaetaeng.xyz/playground' +
           // process.env.VUE_APP_KAKAO_REDIRECT_URL +
-          '&response_type=code&' 
+          '&response_type=code&' +
+          'scope=profile_nickname profile_image'
           window.location.href = url;
 	  
     }
