15738304d38ae77312b07ff827de400fa8bfb7ef
GaeTaeng <nuckly60@gmail.com>
2023-01-16 15:30:54 +0900

    커피페이지 추가

--- src/Components/Contents/PlayGround/Example/ExampleA.js
+++ src/Components/Contents/PlayGround/Example/ExampleA.js
@@ -4,7 +4,8 @@ import React, { useState } from "react";
 export default function ExampleA({ children }) {   
     return (
         <div> 
-            얘처럼 추가하면됩니당 AAA
+            커피 하나만 사서 오겠습니다!
+            필요하면 말씀해주세요!!
         </div>
     )
 }
