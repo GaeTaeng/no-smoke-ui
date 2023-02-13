4ee9c85b58d70b8ca59c54583b0abb473d7a5165
Kim TaeHun <nuckly60@gmail.com>
2023-02-13 13:28:47 +0900

    게시판 택

--- src/Components/Contents/Board/BoardListManagement/BoardListManagement.js
+++ src/Components/Contents/Board/BoardListManagement/BoardListManagement.js
@@ -7,30 +7,7 @@ export default function BoardListManagement({ boardList }) {
         window.location.href = "/board/new"
     }
     return (
-        <div className="boardlistmanagement">  
-        <div className="Description">
-            아래와 같이 총 2가지 방법으로 개발 가능.(더있지만 귀찮..) <br /><br />
-        </div>
-        <div className="Description">
-            1. DIV BootStrap을 이용한 방식
-        </div>
-        
-        
-            <div className="container text-center">
-                <div className="row">
-                    <div className="col col-md-2 title">TITLE</div>
-                    <div className="col col-md-7 title">CONTENTS</div>
-                    <div className="col col-md-1 title">작성자</div>
-                    <div className="col col-md-2 title">작성일</div>
-                </div>
-                {boardList.map((board, idx) => {
-                    console.log("board : ", board)
-                    return <BoardItem key={idx} data={board} />
-                })}
-            </div>  
-            <div className="Description ">
-                2. Table BootStrap을 이용한 방식
-            </div>
+        <div className="boardlistmanagement">   
             
             <table className="table">
             <thead>
